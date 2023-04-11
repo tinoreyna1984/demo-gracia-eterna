86051536eab449a1d04a87b28d669ead3ef48af3
Tino Reyna <tinoreyna1984@gmail.com>
2023-04-11 17:47:25 -0500

    Nuevo avance: dropdown de submenu y mas paginas

--- layout/header.js
+++ layout/header.js
@@ -2,27 +2,46 @@ import Link from "next/link";
 
 export default function Header() {
 
-    const ul = (typeof window !== "undefined") && document.querySelector("ul");
+
+    const mainmenu = (typeof window !== "undefined") && document.querySelector(".menu");
     const check = (typeof window !== "undefined") && document.querySelector("#check");
     const icon = (typeof window !== "undefined") && document.querySelector("#icon");
+    const subMenu = (typeof window !== "undefined") && document.querySelector(".sub-menu");
 
-    const showSidebar = () => {
-        if (check.checked === false && ul.style.left === "-100%") {
+    const hideSidebar = () => {
+        if (check.checked === false && mainmenu.style.left === "-100%") {
             icon.innerHTML = "close";
-            ul.style.left = 0;
+            mainmenu.style.left = 0;
             check.checked = true;
         }
         else {
             icon.innerHTML = "menu";
-            ul.style.left = "";
+            mainmenu.style.left = "";
             check.checked = false;
         }
-        //console.log(check.checked, ul.style.left, icon.innerHTML)
+        //console.log(check.checked, mainmenu.style.left, icon.innerHTML)
     };
 
     const toggleMenu = () => {
         icon.innerHTML = (check.checked === true) ? "close" : "menu";
-        //console.log(check.checked, ul.style.left, icon.innerHTML)
+        //console.log(check.checked, mainmenu.style.left, icon.innerHTML)
+    }
+
+    const toggleSubMenu = () => {
+        if(!window.matchMedia("(max-width: 767px)").matches) return;
+        subMenu.style.left = (subMenu.style.left === "-100%" || subMenu.style.left === "") ? "0" : "-100%";
+    }
+
+    const clickSubMenuItem = () => {
+        if(!window.matchMedia("(max-width: 767px)").matches) return;
+        subMenu.style.removeProperty("left");
+        hideSidebar();
+    }
+
+    const subMenuFocusLost = () => {
+        if(!window.matchMedia("(max-width: 767px)").matches) return;
+        const checkFocus = document.activeElement === subMenu;
+        if(!checkFocus) subMenu.style.removeProperty("left");
     }
 
     return (
@@ -33,14 +52,23 @@ export default function Header() {
                     <span id="icon" className="material-symbols-outlined">menu</span>
                 </label>
                 <Link href={"/"} className="enlace">
-                    <img src="ge-logo.jpg" alt="logo" className="logo" />
+                    <img src="../ge-logo.jpg" alt="logo" className="logo" />
                 </Link>
-                <ul>
-                    <li><Link onClick={showSidebar} href="/">Inicio</Link></li>
-                    <li><Link onClick={showSidebar} href="#">Nuestra iglesia</Link></li>
-                    <li><Link onClick={showSidebar} href="#">Ministerios</Link></li>
-                    <li><Link onClick={showSidebar} href="#">Mensaje del pastor</Link></li>
-                    <li><Link onClick={showSidebar} href="#">Contacto</Link></li>
+                <ul className="menu">
+                    <li><Link onClick={hideSidebar} href="/">Inicio</Link></li>
+                    <li>
+                        <Link onClick={toggleSubMenu} href="#">Nuestra iglesia (+)</Link>
+                        <ul onClick={subMenuFocusLost} className="sub-menu">
+                            <li><Link onClick={clickSubMenuItem} href="/historia/nuestra-historia">Nuestra historia</Link></li>
+                            <li><Link onClick={clickSubMenuItem} href="/historia/reforma">La Reforma Protestante</Link></li>
+                            <li><Link onClick={clickSubMenuItem} href="/historia/iglesia">Iglesia Presbiteriana</Link></li>
+                            <li><Link onClick={clickSubMenuItem} href="/historia/nuestra-fe">Nuestra fe</Link></li>
+                            <li className="md:hidden"><span className="material-symbols-outlined cursor-pointer">close</span></li>
+                        </ul>
+                    </li>
+                    <li><Link onClick={hideSidebar} href="#">Ministerios</Link></li>
+                    <li><Link onClick={hideSidebar} href="#">Servicios</Link></li>
+                    <li><Link onClick={hideSidebar} href="#">Contacto</Link></li>
                 </ul>
             </nav>
         </header>
---
+++ pages/historia/nuestra-historia.js
@@ -0,0 +1,23 @@
+export default function NuestraHistoria() {
+    return (
+        <>
+            <section className='flex flex-col gap-2 mt-24 mb-5 px-6 py-8 lg:px-12 lg:py-0 lg:mt-36 lg:mb-10
+            md:grid md:grid-cols-2
+            lg:grid-cols-2 lg:grid-rows-[100px_200px_200px] lg:items-center scroll-mt-[130px] lg:scroll-mt-[200px]'
+            >
+                <h2 className='text-[28px] md:text-[48px] lg:text-[52px] text-center md:text-start uppercase lg:text-left'>
+                    Nuestra historia
+                </h2>
+                <img className='md:row-span-2 md:col-span-1 lg:self-start'
+                    src='../monumento-reforma.jpg' alt='movimiento-reforma'
+                />
+                <p className='md:text-[16px] lg:text-[24px] lg:self-start'>
+                    Irure sunt id voluptate velit enim ea exercitation nostrud duis dolor non nisi sint. Exercitation qui qui aliquip officia consectetur qui veniam. Sunt velit adipisicing velit elit dolor est id. Veniam ipsum dolore sit exercitation cillum ad id nisi quis anim. Pariatur occaecat duis nostrud sint ullamco labore eu aliqua.
+                </p>
+                <p className='md:text-[16px] lg:text-[24px] lg:self-start'>
+                    Consequat pariatur incididunt ea qui quis nostrud pariatur. Ex anim qui incididunt sit veniam sint Lorem. Nisi irure enim do enim excepteur sint occaecat irure reprehenderit duis mollit. Quis consectetur anim Lorem cupidatat non. Lorem velit anim id ut non. Dolor aute incididunt mollit sint ad minim Lorem aliqua sint nostrud aliqua nisi in.
+                </p>
+            </section>
+        </>
+    );
+}
