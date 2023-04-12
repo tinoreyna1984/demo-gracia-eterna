cc311ece8ff34a556acb7a84eb5fed210671cccf
Tino Reyna <tinoreyna1984@gmail.com>
2023-04-12 15:47:48 -0500

    avance de contenido

--- pages/historia/nuestra-fe.js
+++ pages/historia/nuestra-fe.js
@@ -2,20 +2,37 @@ export default function NuestraFe() {
     return (
         <>
             <section className='flex flex-col gap-2 mt-24 px-6 py-8 lg:px-12 lg:py-0 lg:mt-36
-            md:grid md:grid-cols-2
-            lg:grid-cols-2 lg:grid-rows-[100px_200px_200px] lg:items-center scroll-mt-[130px] lg:scroll-mt-[200px]'
+                scroll-mt-[130px] lg:scroll-mt-[200px]'
             >
                 <h2 className='text-[28px] md:text-[48px] lg:text-[52px] text-center md:text-start uppercase lg:text-left'>
                     Nuestra fe
                 </h2>
-                <img className='md:row-span-2 md:col-span-1 lg:self-start'
-                    src='../monumento-reforma.jpg' alt='movimiento-reforma'
-                />
-                <p className='md:text-[16px] lg:text-[24px] lg:self-start'>
-                    Irure sunt id voluptate velit enim ea exercitation nostrud duis dolor non nisi sint. Exercitation qui qui aliquip officia consectetur qui veniam. Sunt velit adipisicing velit elit dolor est id. Veniam ipsum dolore sit exercitation cillum ad id nisi quis anim. Pariatur occaecat duis nostrud sint ullamco labore eu aliqua.
-                </p>
-                <p className='md:text-[16px] lg:text-[24px] lg:self-start'>
-                    Consequat pariatur incididunt ea qui quis nostrud pariatur. Ex anim qui incididunt sit veniam sint Lorem. Nisi irure enim do enim excepteur sint occaecat irure reprehenderit duis mollit. Quis consectetur anim Lorem cupidatat non. Lorem velit anim id ut non. Dolor aute incididunt mollit sint ad minim Lorem aliqua sint nostrud aliqua nisi in.
+                {/* <img className='md:row-span-2 md:col-span-1'
+                    src='../historia/nuestra-fe/martin-lutero.jpg' alt='lutero' title="Martín Lutero"
+                /> */}
+                <h3 className='text-[20px] md:text-[40px] lg:text-[32px] text-center md:text-start uppercase lg:text-left mt-6'>
+                    Las 5 solas de la Reforma Protestante
+                </h3>
+                <ul className="list-disc ml-6 md:text-[16px] lg:text-[24px] pb-10">
+                    <li><span className="font-bold">Sola fide</span>: la salvación es solo por fe en Cristo Jesús (Hechos 16:31)</li>
+                    <li><span className="font-bold">Sola gratia</span>: la salvación y todas las bondades de este tiempo son solo por la gracia de Dios (Efesios 2:8-9, Mateo 5:45)</li>
+                    <li><span className="font-bold">Sola Scriptura</span>: la autoridad máxima de la teología y la práctica del creyente y de la iglesia es la Biblia, la cual es la Palabra de Dios, siendo esta divinamente inspirada, inerrante, inefable y suficiente (2 Timoteo 3:16-17, Deuteronomio 29:29)</li>
+                    <li><span className="font-bold">Solus Christus</span>: la salvación es solamente por la obra redentora de Jesús el Cristo. En Él y en Su nombre confiamos nuestra salvación, no habiendo otro camino para llegar al Padre (Juan 14:6, Hechos 4:12)</li>
+                    <li><span className="font-bold">Soli Deo Gloria</span>: la vida del creyente cristiano es para la gloria de Dios (1 Corintios 10:31)</li>
+                </ul>
+                <h3 className='text-[20px] md:text-[40px] lg:text-[32px] text-center md:text-start uppercase lg:text-left mt-6'>
+                    Documentos históricos que definen nuestra fe
+                </h3>
+                <p className='md:text-[16px] lg:text-[24px] pb-10'>Con base en las Escrituras, la Iglesia Reformada Presbiteriana sostiene el legado de la obra cristiana y reformada, la cual está reflejada en los siguientes documentos históricos:</p>
+                <ul className="list-disc ml-6 md:text-[16px] lg:text-[24px] pb-10">
+                    <li>Los credos de la historia de la iglesia: Credo Apostólico, Credo Niceno y Credo Atanasiano</li>
+                    <li>Documentos de fe reformada: Catecismo de Heidelberg, la Confesión Belga y los Cánones de Dort</li>
+                </ul>
+                <h3 className='text-[20px] md:text-[40px] lg:text-[32px] text-center md:text-start uppercase lg:text-left mt-6'>
+                    Nuestra declaración de fe
+                </h3>
+                <p className='md:text-[16px] lg:text-[24px] pb-10'>
+                    (Próximamente)
                 </p>
             </section>
         </>
--- pages/historia/reforma.js
+++ pages/historia/reforma.js
@@ -2,20 +2,40 @@ export default function Reforma() {
     return (
         <>
             <section className='flex flex-col gap-2 mt-24 px-6 py-8 lg:px-12 lg:py-0 lg:mt-36
-            md:grid md:grid-cols-2
-            lg:grid-cols-2 lg:grid-rows-[100px_200px_200px] lg:items-center scroll-mt-[130px] lg:scroll-mt-[200px]'
+                scroll-mt-[130px] lg:scroll-mt-[200px]'
             >
                 <h2 className='text-[28px] md:text-[48px] lg:text-[52px] text-center md:text-start uppercase lg:text-left'>
                     La Reforma Protestante
                 </h2>
-                <img className='md:row-span-2 md:col-span-1 lg:self-start'
-                    src='../monumento-reforma.jpg' alt='movimiento-reforma'
-                />
-                <p className='md:text-[16px] lg:text-[24px] lg:self-start'>
-                    Irure sunt id voluptate velit enim ea exercitation nostrud duis dolor non nisi sint. Exercitation qui qui aliquip officia consectetur qui veniam. Sunt velit adipisicing velit elit dolor est id. Veniam ipsum dolore sit exercitation cillum ad id nisi quis anim. Pariatur occaecat duis nostrud sint ullamco labore eu aliqua.
+                <p className='md:text-[16px] lg:text-[24px]'>
+                    Es el evento histórico que ha impactado profundamente al mundo, el cual parte de Europa Occidental y ha propiciado la difusión más amplia de la Biblia y ha acelerado el progreso social del viejo continente. Efectos que se mantienen al día de hoy a nivel mundial.
                 </p>
-                <p className='md:text-[16px] lg:text-[24px] lg:self-start'>
-                    Consequat pariatur incididunt ea qui quis nostrud pariatur. Ex anim qui incididunt sit veniam sint Lorem. Nisi irure enim do enim excepteur sint occaecat irure reprehenderit duis mollit. Quis consectetur anim Lorem cupidatat non. Lorem velit anim id ut non. Dolor aute incididunt mollit sint ad minim Lorem aliqua sint nostrud aliqua nisi in.
+                <p className='md:text-[16px] lg:text-[24px]'>
+                    Sucede esto un 31 de octubre de 1517, en Wittenberg, Alemania. Martín Lutero (1483 - 1546), quien era -en ese entonces- un monje agustino, entendió el vacío de su vida que no podía ser cubierto por su vida monástica. Al entrar a la Universidad de Wittenberg para estudiar teología, encontró la clave en el estudio de la Carta a los Romanos, muy particularmente el verso 1:17, el cual dice: "Porque en el evangelio la justicia de Dios se revela por fe y para fe, como está escrito: Mas el justo por la fe vivirá." Esto insertó un consuelo en el alma de este religioso y genera una nueva perspectiva que le llevaría al redescubrimiento de la Palabra de Dios en una Europa humanista temprana.
+                </p>
+                <img className='md:row-span-2 md:col-span-1'
+                    src='../historia/reforma/martin-lutero.jpg' alt='lutero' title="Martín Lutero"
+                />
+                <p className='md:text-[16px] lg:text-[24px]'>
+                    Sin embargo, a pesar de que el deseo de este ex-monje ha sido el de restaurar la iglesia con su teología, le valió un enfrentamiento con el papa León X y su eventual excomunión, debido a la denuncia que Lutero hizo a través de sus "95 Tesis contra la venta de indulgencias", pues este emprendimiento católico romano era el punto cumbre de la corrupción institucional y moral de la iglesia. Una indulgencia era un documento eclesial por el cual el hombre tiene acceso directo al cielo sin pasar por el purgatorio, y el papa se valió de esto para procurar resolver la crisis financiera en Roma.
+                </p>
+                <p className='md:text-[16px] lg:text-[24px]'>
+                    En este punto, se generó un nuevo cisma en la iglesia, cuyo precedente más cercano fue en el año 1054, el "gran cisma de la iglesia". Lutero comenzó a diseminar sus trabajos en Alemania y estados directamente asociados al Sacro Imperio Germánico. Subsecuentemente, los príncipes europeos que decidieron aceptar la teología luterana y deslindarse del papa emitieron una protesta en la llamada Dieta de Spira (1529) ante el entonces emperador Carlos V. De ahí a los términos "protestantismo" y "protestante", entre otras variaciones.
+                </p>
+                <p className='md:text-[16px] lg:text-[24px]'>
+                    Este proceso involucró a otras personas prominentes del continente, entre ellas Juan Calvino (1509 - 1564), quien ha establecido el tratado teológico más grande del protestantismo: la "Institución de la Religión Cristiana". Es la base de la teología reformada y una obra emblemática cuyos preceptos puso en práctica en su ministerio en Ginebra, Suiza.
+                </p>
+                <img className='md:row-span-2 md:col-span-1'
+                    src='../historia/reforma/juan-calvino.jpg' alt='juan calvino' title="Juan Calvino"
+                />
+                <p className='md:text-[16px] lg:text-[24px]'>
+                    En resumen, fue en aquel periodo en el que los llamados reformadores fueron llamados por Dios para buscar el reencuentro con Dios a través de la Biblia, que trajo consigo nuevas perspectivas de vida cristiana, misión, teología, etcétera, para la gloria de Dios.
+                </p>
+                <h3 className='text-[20px] md:text-[40px] lg:text-[32px] text-center md:text-start uppercase lg:text-left'>
+                    La Reforma hoy
+                </h3>
+                <p className='md:text-[16px] lg:text-[24px] pb-10'>
+                    El legado de la reforma continúa al día de hoy, pues a través de los tiempos la iglesia se renueva cada día según la Palabra de Dios. "Ecclesia reformata semper reformanda est" ("La iglesia reformada siempre está reformándose").
                 </p>
             </section>
         </>
